7aacc01088f7b322f365964637b1caccfed6eef0
eevisk <eeva.kivila@gmail.com>
2024-05-02 19:32:21 +0300

    kissa liikkuu

---
+++ Assets/My Assets/Scripts/cat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    private float verticalSpeed = 0f;
+    private float horizontalSpeed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalSpeed = Input.GetAxis("Vertical");
+        horizontalSpeed = Input.GetAxis("Horizontal");
+
+        Vector3 direction = new(horizontalSpeed, -1, verticalSpeed);
+
+        controller.Move(direction * Time.deltaTime);
+    }
+}
